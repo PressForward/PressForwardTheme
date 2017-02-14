a6c64bd47f2d20f3481c738859cf3aa79827ae1a
Amanda Regan <regan008@cougars.csusm.edu>
2017-02-14 13:33:32 -0500

    finish out the layout for first block.

--- page-homepage.php
+++ page-homepage.php
@@ -13,16 +13,34 @@ Template Name: Homepage
           <h1>PressForward Builds Content Hubs</h1>
           <h2>Centralize digital media into a single online resource.</h2>
           <div class="row">
-          <div class="large-4 columns block1-circles">
-            <h3>Gather</h3>
+          <div class="large-4 columns block1-circles text-center">
+						<span class="fa-stack fa-2x">
+							<i class="fa fa-square fa-stack-2x"></i>
+							<i class="fa fa-rss fa-stack-1x fa-inverse"></i>
+						</span>
+						<h3>Gather</h3>
+					  <p>Use the bookmarklet and feed reader to collect content from around the web.</p>
+		</div>
+          <div class="large-4 columns block1-circles text-center">
+						<span class="fa-stack fa-2x">
+  						<i class="fa fa-square fa-stack-2x"></i>
+  						<i class="fa fa-filter fa-stack-1x fa-inverse"></i>
+						</span>
+						<h3>Organize</h3>
+						<p>Filter, sort, and select content in your WordPress dashboard.</p>
           </div>
-          <div class="large-4 columns block1-circles">
-            <h3>Organize</h3>
-          </div>
-          <div class="large-4 columns block1-circles">
+          <div class="large-4 columns block1-circles text-center">
+						<span class="fa-stack fa-2x">
+  						<i class="fa fa-square fa-stack-2x"></i>
+  						<i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+						</span>
             <h3>Publish</h3>
+						<p>Amplify and share the content that matters to you.</p>
           </div>
         </div>
+				<div class="row text-center">
+					<a class="button primary large" href="#">Download the Free WordPress Plugin</a>
+				</div>
 
 
 			</div> <!-- end #block1 -->
