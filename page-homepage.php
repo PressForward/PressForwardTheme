4907f2abd5a09a0f8ef7806970130c2392c90c54
Amanda Regan <regan008@cougars.csusm.edu>
2017-02-14 12:54:50 -0500

    basic setup

---
+++ page-homepage.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Homepage
+*/
+?>
+<?php get_header(); ?>
+
+	<div id="content">
+
+		<div id="inner-content" class="expanded row">
+
+		    <div id="block1" class="large-12 medium-12 columns" role="main">
+          <h1>PressForward Builds Content Hubs</h1>
+          <h2>Centralize digital media into a single online resource.</h2>
+          <div class="row">
+          <div class="large-4 columns block1-circles">
+            <h3>Gather</h3>
+          </div>
+          <div class="large-4 columns block1-circles">
+            <h3>Organize</h3>
+          </div>
+          <div class="large-4 columns block1-circles">
+            <h3>Publish</h3>
+          </div>
+        </div>
+
+
+			</div> <!-- end #block1 -->
+
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
