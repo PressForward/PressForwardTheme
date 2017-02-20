c02f918972412060f664c72e71f0c90cf5dc10fc
Amanda Regan <aregan2@gmu.edu>
2017-02-20 11:35:28 -0500

    outline of homepage

--- page-homepage.php
+++ page-homepage.php
@@ -10,42 +10,104 @@ Template Name: Homepage
 		<div id="inner-content" class="expanded row">
 
 		    <div id="block1" class="large-12 medium-12 columns" role="main">
-          <h1>PressForward Builds Content Hubs</h1>
-          <h2>Centralize digital media into a single online resource.</h2>
-          <div class="row">
-          <div class="large-4 columns block1-circles text-center">
-						<span class="fa-stack fa-2x">
-							<i class="fa fa-square fa-stack-2x"></i>
-							<i class="fa fa-rss fa-stack-1x fa-inverse"></i>
-						</span>
-						<h3>Gather</h3>
-					  <p>Use the bookmarklet and feed reader to collect content from around the web.</p>
-		</div>
-          <div class="large-4 columns block1-circles text-center">
-						<span class="fa-stack fa-2x">
-  						<i class="fa fa-square fa-stack-2x"></i>
-  						<i class="fa fa-filter fa-stack-1x fa-inverse"></i>
-						</span>
-						<h3>Organize</h3>
-						<p>Filter, sort, and select content in your WordPress dashboard.</p>
-          </div>
-          <div class="large-4 columns block1-circles text-center">
-						<span class="fa-stack fa-2x">
-  						<i class="fa fa-square fa-stack-2x"></i>
-  						<i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
-						</span>
-            <h3>Publish</h3>
-						<p>Amplify and share the content that matters to you.</p>
-          </div>
-        </div>
+          	<h1>PressForward Builds Content Hubs</h1>
+          	<h2>Centralize digital media into a single online resource.</h2>
+
+						<div class="row">
+          			<div class="large-4 columns block1-circles text-center">
+										<span class="fa-stack fa-2x">
+											<i class="fa fa-square fa-stack-2x"></i>
+											<i class="fa fa-rss fa-stack-1x fa-inverse"></i>
+										</span>
+
+										<h3>Gather</h3>
+					  				<p>Use the bookmarklet and feed reader to collect content from around the web.</p>
+								</div>
+
+								<div class="large-4 columns block1-circles text-center">
+										<span class="fa-stack fa-2x">
+				  						<i class="fa fa-square fa-stack-2x"></i>
+				  						<i class="fa fa-filter fa-stack-1x fa-inverse"></i>
+										</span>
+										<h3>Organize</h3>
+										<p>Filter, sort, and select content in your WordPress dashboard.</p>
+          			</div>
+
+								<div class="large-4 columns block1-circles text-center">
+										<span class="fa-stack fa-2x">
+				  						<i class="fa fa-square fa-stack-2x"></i>
+				  						<i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+										</span>
+				            <h3>Publish</h3>
+										<p>Amplify and share the content that matters to you.</p>
+          			</div>
+        		</div>
+
 				<div class="row text-center">
 					<a class="button primary large" href="#">Download the Free WordPress Plugin</a>
 				</div>
 
 
-			</div> <!-- end #block1 -->
+			</div> <!-- end #block1 (large 12 columns) -->
 
-		</div> <!-- end #inner-content -->
+		</div> <!-- end #inner-content (row) -->
+
+		<div class="row">
+			<div class="large-12 columns">
+				<h2>Gather</h2>
+			</div>
+			<div class="large-8 medium-8 small-12 columns">
+				<ul>
+					<li>Add feeds to the RSS reader to bring all your content to PressForward.</li>
+					<li>Grab content from around the web with the "Nominate This" bookmarklet.</li>
+				</ul>
+			</div>
+			<div class="large-4 medium-4 small-12 columns">
+				<p>image goes here</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="large-12 columns ">
+				<h2>Organize</h2>
+			</div>
+			<div class="large-4 medium-4 small-12 columns">
+				<p>image goes here</p>
+			</div>
+			<div class="large-8 medium-8 small-12 columns">
+				<ul>
+					<li>Read and review content without leaving your WordPress dashboard.</li>
+					<li>Select and draft the posts you want to share with your community</li>
+					<li>Use folders to group feeds and filter posts</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="large-12 columns">
+				<h2>Publish</h2>
+			</div>
+			<div class="large-8 medium-8 small-12 columns">
+				<ul>
+					<li>Preserve original metadata and redirect to the source.</li>
+					<li>Amplify chosen content by publishing it to your site or sharing it on twitter.</li>
+					<li>Engage your community by featuring recent and relevant content all in one place.</li>
+				</ul>
+			</div>
+			<div class="large-4 medium-4 small-12 columns">
+				<p>image goes here</p>
+			</div>
+		</div>
+
+		<div clas="row">
+			<div class="large-12 columns">
+				<h2>Use Cases</h2>
+			</div>
+		</div>
+		</div>
+
+
+
 
 	</div> <!-- end #content -->
 
