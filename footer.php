192d99b8c958f7acb76fadac163c3fdc60654b5f
Amanda Regan <regan008@cougars.csusm.edu>
2017-05-15 10:48:04 -0400

    fix headings in footer to correspond w/logo

--- footer.php
+++ footer.php
@@ -3,11 +3,11 @@
 						<div class="large-12 columns">
 							<div class="row">
 							<div class="large-4 medium-4 text-center columns" role="main">
-								<h4> Generously Funded By:</h4>
+								<h4>A Project Of:</h4>
 								<a href="https://chnm.org" target="_blank"><img width="250" class="footer-logos" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/rrchnm-logo.png';?>"></a>
 							</div>
 							<div class="large-4 medium-4 text-center columns" role="main">
-								<h4>A Project of:</h4>
+								<h4>Generously Funded By:</h4>
 								<a href="https://sloan.org/" target="_blank"><img width="275" class="footer-logos" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Sloan_Logo.png';?>"></a>
 							</div>
 							<div class="large-4 medium-4 text-center columns">
