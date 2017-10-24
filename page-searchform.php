1bac5adffd715e7509f8bb324ee540e9233169f2
Mandy Regan <Mandy@MacBook-Pro-51.local>
2017-10-24 10:40:59 -0400

    add search page and modify template to show excerpt rather than full text. Change name of full width template to match other template files.

---
+++ page-searchform.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Search Form
+*/
+?>
+
+<?php get_header(); ?>
+
+	<div id="content">
+
+		<div id="inner-content" class="row">
+
+		    <main id="main" class="large-offset-1 large-10 medium-10 large-offset-1 columns" role="main">
+
+				<?php get_search_form(); ?>
+
+			</main> <!-- end #main -->
+
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
